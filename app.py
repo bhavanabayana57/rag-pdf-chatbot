876009d29c1919f4d4c463d93e370158f361dcb6
Bhavana Bayana <bhavanabayana57@gmail.com>
2026-05-30 16:06:26 +0530

    Added Streamlit secrets support

--- app.py
+++ app.py
@@ -127,10 +127,13 @@ if "share" in query_params:
 
 load_dotenv()
 
-# -------------------- GROQ CLIENT --------------------
+groq_key = st.secrets.get(
+    "GROQ_API_KEY",
+    os.getenv("GROQ_API_KEY")
+)
 
 client = OpenAI(
-    api_key=os.getenv("GROQ_API_KEY"),
+    api_key=groq_key,
     base_url="https://api.groq.com/openai/v1"
 )
 
